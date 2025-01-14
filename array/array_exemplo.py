6d576b52518176d1225662a3fa593df6d8dc0987
victor <victorhugoocordeirorocha@gmail.com>
2025-01-14 17:50:16 -0400

    Array

--- array/array_exemplo.py
+++ array/array_exemplo.py
@@ -3,8 +3,8 @@ import array
 # Array de assentos (0 = livre, 1 = ocupado)
 assentos = array.array('i', [0, 1, 0, 1, 0])
 
-# Verificar se o assento 3 está ocupado
+# Verificando se o assendo 3 está ocupado.
 if assentos[2] == 0:
-    print("O assento 3 está livre!")
+    print("O assento está livre!")
 else:
-    print("O assento 3 está ocupado.")
+    print("O assento está ocupado.")
