8c7220d1df82b840bfa28e084e0312143f6e80dc
victor <victorhugoocordeirorocha@gmail.com>
2025-01-14 21:29:22 -0400

    listas

---
+++ listas/listas.py
@@ -0,0 +1,11 @@
+# Lista vazia
+lista_vazia = []
+
+# Lista de inteiros
+numeros = [1, 2, 3, 4]
+
+# Lista mista (com diferentes tipos de dados)
+lista_mista = [10, "Python", 3.14, True]
+
+# Lista aninhada (contém outras listas)
+lista_aninhada = [[1, 2], [3, 4], [5, 6]]
